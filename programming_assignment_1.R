8f6dd86d3c16e3e9e5efa30e5ec6efa63982c2c7
Brock <buckley@Buckleys-MacBook-Pro.local>
2021-04-06 10:53:47 -0700

    part 1 functional commit :)

--- programming_assignment_1.R
+++ programming_assignment_1.R
@@ -1,10 +1,12 @@
 #PART 1
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
- file <- gzfile("specdata")
-  for(i in id) {
-    rbind(x, read.csv(cat(directory,"/",i,".csv")))
+  analysis_data <- data.frame()
+  data_frame_names <- list.files(pattern = "*.csv")
+  for(i in id){
+  analysis_data <- rbind(read.csv(data_frame_names[i]),analysis_data)
   }
+  mean(analysis_data[ , pollutant], na.rm = TRUE)
 }
 
 #PART 2
